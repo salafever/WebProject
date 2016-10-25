59b94e3bbdd59e93e931f103801c556b6837f775
kilhalverson <kilhalverson@mail.roanoke.edu>
2016-10-25 17:06:06 -0400

    populated admin list of names

--- jsonPart.js
+++ jsonPart.js
@@ -1,33 +1,65 @@
 var httpRequest;
 
-var handleResponse = function() {
-    if ((httpRequest.readState === XMLHttpRequest.DONE) &&
-    (httpRequest.status === 200)) {
+//PRE:
+//POST:
+// var handleResponse = function(){
+//     if (httpRequest.readyState == XMLHttpRequest.DONE && httpRequest.status == 200){
+// 	var infoString = httpRequest.responseText;
+// 	console.log(infoString);
 
-    // This will give you the data given
-    //console.log(httpRequest.responseText);
+// 	var infoObject = JSON.parse(infoString);
+// 	console.log(infoObject);
+//     }
+// }
+
+
+
+// //PRE: the id of the element clicked on
+// //POST: changes the contents of the 'personInfo' div to contain
+// //      the information about the matching name
+// var displayInfo = function(event){
+//     var name = event.target.textContent;
+//     console.log(name.innerHTML);
+
+//     httpRequest = new XMLHttpRequest();
+//     httpRequest.onreadystatechange = handleResponse;
+//     httpRequest.open('GET', 'http://cs.roanoke.edu/~kilhalverson/website/dummy.json', true);
+//     httpRequest.send();
     
-    uselessObject = JSON.parse(httpRequest.responseText)
-    var theDiv = document.getElementById('thDiv')
-    var textNode = document.creatTextNode(uselessObject.aString);
 
-    // This tells you that you are ready when clicked.
-    // console.log('I am ready');
-    }
-}
+// }
 
-var buttonClicked = function () {
-    console.log('button clicked')
-    httpRequest.send();
-}
+// var init = function(){
 
-var init = function () {
-    var theButton = document.getElementById('theButton');
-    theButton.addEventListener('click', buttonClicked)
+// }
+// var handleResponse = function() {
+//     if ((httpRequest.readState === XMLHttpRequest.DONE) &&
+//     (httpRequest.status === 200)) {
 
-    httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = handleResponse;
-    httpRequest.open('GET', "https://home/stephanie/public_html/project/dataStorage.json", true);
+//     // This will give you the data given
+//     //console.log(httpRequest.responseText);
     
-}
+//     uselessObject = JSON.parse(httpRequest.responseText)
+//     var theDiv = document.getElementById('thDiv')
+//     var textNode = document.creatTextNode(uselessObject.aString);
+
+//     // This tells you that you are ready when clicked.
+//     // console.log('I am ready');
+//     }
+// }
+
+// var buttonClicked = function () {
+//     console.log('button clicked')
+//     httpRequest.send();
+// }
+
+// var init = function () {
+//     var theButton = document.getElementById('theButton');
+//     theButton.addEventListener('click', buttonClicked)
+
+//     httpRequest = new XMLHttpRequest();
+//     httpRequest.onreadystatechange = handleResponse;
+//     httpRequest.open('GET', "https://home/stephanie/public_html/project/dataStorage.json", true);
+    
+// }
 
