999487ebc6e0d53e0b58a6886876c5385bfadd34
Stephanie LaFever <salafever@mail.roanoke.edu>
2016-10-25 20:11:12 -0400

    minor changes

--- jsonPart.js
+++ jsonPart.js
@@ -1,33 +1,39 @@
-var httpRequest;
+// var httpRequest;
 
-var handleResponse = function() {
-    if ((httpRequest.readState === XMLHttpRequest.DONE) &&
-    (httpRequest.status === 200)) {
+// var handleResponse = function() {
+//     if ((httpRequest.readState === XMLHttpRequest.DONE) &&
+//     (httpRequest.status === 200)) {
 
-    // This will give you the data given
-    //console.log(httpRequest.responseText);
+//     // This will give you the data given
+//     //console.log(httpRequest.responseText);
     
-    uselessObject = JSON.parse(httpRequest.responseText)
-    var theDiv = document.getElementById('thDiv')
-    var textNode = document.creatTextNode(uselessObject.aString);
+//     uselessObject = JSON.parse(httpRequest.responseText)
+//     var theDiv = document.getElementById('thDiv')
+//     var textNode = document.creatTextNode(uselessObject.aString);
 
-    // This tells you that you are ready when clicked.
-    // console.log('I am ready');
-    }
-}
+//     // This tells you that you are ready when clicked.
+//     // console.log('I am ready');
+//     }
+// }
 
-var buttonClicked = function () {
-    console.log('button clicked')
-    httpRequest.send();
-}
+// var buttonClicked = function () {
+//     console.log('button clicked')
+//     httpRequest.send();
+// }
 
-var init = function () {
-    var theButton = document.getElementById('theButton');
-    theButton.addEventListener('click', buttonClicked)
+// var init = function () {
+//     var theButton = document.getElementById('theButton');
+//     theButton.addEventListener('click', buttonClicked)
 
-    httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = handleResponse;
-    httpRequest.open('GET', "https://home/stephanie/public_html/project/dataStorage.json", true);
+//     httpRequest = new XMLHttpRequest();
+//     httpRequest.onreadystatechange = handleResponse;
+//     httpRequest.open('GET', "https://home/stephanie/public_html/project/dataStorage.json", true);
     
-}
+// }
+
+var namespace = {};
+
+namespace.xhtml = new XMLHttpRequest();
+
+namespace.url = "adminTable.json"
 
