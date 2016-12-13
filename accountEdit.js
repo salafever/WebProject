917b4d6666c0db6d550acbd9a2f80cbadc66ea02
kilhalverson <kilhalverson@mail.roanoke.edu>
2016-12-13 14:36:43 -0500

    more changes

--- accountEdit.js
+++ accountEdit.js
@@ -3,38 +3,89 @@
 //Update Account
 
 $(document).ready(function(){
+    //ASSERT: the document loaded so we can populate the
+    //        account
+    
+    $.ajax({
+	url: 'getUserInfo.php',
+	data: {'cookie': true},
+	type: 'POST',
+	//ASSERT: http request is made sending over
+	//        the user information
+
+	success: function(data){
+	    var userInformation = response;
+	}
+    });
+    
+
+    console.log(userInformation);
 
 
+    
+
+    
 
 
     $("#submitButt").click(function(){
-	//ASSERT: the sign up button has been clicked
+	//ASSERT: the account update button has been clicked
 	
 	var name = document.getElementById("name").value;
 	var email = document.getElementById("email").value;
 	var city = document.getElementById("city").value;
 	var state = document.getElementById("state").value;
 
-
-
-
-
+	var ma1 = document.getElementById("ma1").value;
+	var ma2 = document.getElementById("ma2").value;
+	var ma3 = document.getElementById("ma3").value;
+	var mi1 = document.getElementById("mi1").value;
+	var mi2 = document.getElementById("mi2").value;
+	var mi3 = document.getElementById("mi3").value;
 	
-	//ASSERT: the input data taken from the name, email, password
-	
-	var userObject =
+	var title = document.getElementById("title").value;
+	var phone = document.getElementById("phone").value;
+	var linkedin = document.getElementById("linkedin").value;
+
+	var desc = document.getElementById("desc").value;
+	var advanced = document.getElementById("advanced").value;
+
+	var gradMon = document.getElementById("gradMon").value;
+	var gradYear = document.getElementById("gradYear").value;
+
+	//ASSERT: the input data taken from the account setting
+	//        fields
+	var updateObject =
 	    {
 		"name": name,
 		"email": email,
-		"password": password
+		"city": city,
+		"state": state,
+
+		"ma1": ma1,
+		"ma2": ma2,
+		"ma3": ma3,
+
+		"mi1": mi1,
+		"mi2": mi2,
+		"mi3": mi3,
+
+		"title": title,
+		"phone": phone,
+		"linkedin": linkedin,
+
+		"desc": desc,
+		"advanced": advanced,
+
+		"gradMon": gradMon,
+		"gradYear": gradYear
 	    }
 	//ASSERT: information placed into a JSON object
 	
-	var userString = JSON.stringify(userObject);
+	var updateString = JSON.stringify(userObject);
 	//ASSERT: the object is converted to a string
 	
 	$.ajax({
-	    url: 'signupValid.php',
+	    url: 'updateUser.php',
 	    data: {'aUser': userString},
 	    type: 'POST'
 	    //ASSERT: http request is made sending over
--- getUserInfo.php
+++ getUserInfo.php
@@ -1,35 +1,57 @@
 
+//Kimi Halverson
+//PHP Script to get a user from the database given the name
+
 <?php
 
-
-
-$usercookie = $_COOKIE['usercookie'];
-
 $database = simplexml_load_file("database.xml");
+//ASSERT: load up the database
+
+$foundUser;
+//ASSERT: to store the user we're looking for
 
 foreach($database->alum as $anAlum){
-    if($usercookie == $anAlum->usercookie){
-        $name = $anAlum->name;
-        $email = $anAlum->email;
-        $city = $anAlum->city;
-        $state = $anAlum->state;
-        $ma1 = $anAlum->ma1;
-        $ma2 = $anAlum->ma2;
-        $ma3 = $anAlum->ma3;
-        $mi1 = $anAlum->mi1;
-        $mi2 = $anAlum->mi2;
-        $mi3 = $anAlum->mi3;
-
-        $title = $anAlum->title;
-        $phone = $anAlum->phone;
-        $linkedin = $anAlum->linkedin;
-        $desc = $anAlum->desc;
-        $advanced = $anAlum->advanced;
-
-        $gradMon = $anAlum->gradMon;
-        $gradYear = $anAlum->gradYear;
+    if($_POST['cookie'] == true){
+        $usercookie = $_COOKIE['usercookie'];
+        if($usercookie == $anAlum->usercookie){
+            $foundUser = $anAlum;
+        }
+    }
+    else if{
+        if($_POST['name'] == $anAlum->name){
+            $foundUser = $anAlum;
+            //ASSERT: there's a name that matches in the
+            //        database
+        }
     }
 }
 
+
+$user = array(
+    //ASSERT: create an array out of the
+    //        info from the database
+    "name" => $foundUser->name;
+    "email" => $foundUser->email;
+    "city" => $foundUser->city;
+    "state" => $foundUser->state;
+    "ma1" => $foundUser->ma1;
+    "ma2" => $foundUser->ma2;
+    "ma3" => $foundUser->ma3;
+    "mi1" => $foundUser->mi1;
+    "mi2" => $foundUser->mi2;
+    "mi3" => $foundUser->mi3;
+    
+    "title" => $foundUser->title;
+    "phone" => $foundUser->phone;
+    "linkedin" => $foundUser->linkedin;
+    "desc" => $foundUser->desc;
+    "advanced" => $foundUser->advanced;
+    
+    "gradMon" => $foundUser->gradMon;
+    "gradYear" => $foundUser->gradYear;
+);
+
+
+echo json_encode($user);
 
 ?>
