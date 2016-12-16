ce40a61140238d6ee8068b5a135a02b4d0758850
Stephanie LaFever <salafever@mail.roanoke.edu>
2016-12-15 21:37:02 -0500

    fixing map functions

--- loginmap.js
+++ loginmap.js
@@ -2,10 +2,31 @@
 
 // PRE: takes the latitude, longitude, city, state, job title, name, and nameID
 //       of one given entry/person.
-// POST: creates the marker on the map for the person given and creates the
-//        box for the individual. 
-var createMarker = function (aLatitude, aLongitude, aCity, aState, jobTitle,
-			     aName, nameID) {
+// POST: creates the map with all of the markers on the map created. 
+var myMap = function(aLatitude, aLongitude,  aCity, aState, jobTitle, aName,
+		     nameID) {
+
+    var mapOptions = {
+        zoom: 4,
+	center: new google.maps.LatLng(37.850033, -95.6500523),
+	//ASSERT: positioning of the map
+	
+	scrollwheel: false,
+	draggable: false,
+	disableDefaultUI: true,
+	//ASSERT: turn off zoom/drag control and remove the
+	//        interface
+	
+        styles: [{"stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#2f343b"}]},{"featureType":"landscape","stylers":[{"visibility":"on"},{"color":"#cccccc"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#2f343b"},{"weight":0}]}]
+	//ASSERT: style of the map
+    };
+
+    var mapCanvas = document.getElementById("map");
+    //ASSERT: get the element of the map div
+
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    //ASSERT: create a map object
+
 
     var contentString = '<div id="content">'+
       '<div id="siteNotice">'+
@@ -35,37 +56,12 @@ var createMarker = function (aLatitude, aLongitude, aCity, aState, jobTitle,
 	infowindow.open(map, marker);
     });
     //ASSERT: makes it capable to click on the marker to see text box
+
+    marker.setMap(map);
     
-}
-
-// PRE: The map function was called inside the html pages.
-// POST: creates the map with all of the markers on the map created. 
-var myMap = function(aLatitude, aLongitude,  aCity,
-		      aState, jobTitle, aName, nameID) {
-
-    var mapOptions = {
-        zoom: 4,
-	center: new google.maps.LatLng(37.850033, -95.6500523),
-	//ASSERT: positioning of the map
-	
-	scrollwheel: false,
-	draggable: false,
-	disableDefaultUI: true,
-	//ASSERT: turn off zoom/drag control and remove the
-	//        interface
-	
-        styles: [{"stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#2f343b"}]},{"featureType":"landscape","stylers":[{"visibility":"on"},{"color":"#cccccc"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#2f343b"},{"weight":0}]}]
-	//ASSERT: style of the map
-    };
-
-    var mapCanvas = document.getElementById("map");
-    //ASSERT: get the element of the map div
-
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    //ASSERT: create a map object
-   
+    
     // =======================================================
-
+    
     
     //Use Javascript to make an AJAX call to PHP asking for the
     //cities and states of everyone in the database
@@ -83,7 +79,6 @@ var myMap = function(aLatitude, aLongitude,  aCity,
     //Use a for loop, using the function createMarker function
     
     // To add the marker to the map, call setMap();
-    marker.setMap(map);
 
     
 }
