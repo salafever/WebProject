ce856f4e28f60d385f776ded474d34f6d559e352
Stephanie LaFever <salafever@mail.roanoke.edu>
2016-12-16 04:13:27 -0500

    signup

--- signupAccount.js
+++ signupAccount.js
@@ -1,41 +1,56 @@
 
+var namespace = {};
+namespace.success = 0;
+namespace.response;
+
 //Author: Kimi Halverson
 //Sign up Validation
 
 $(document).ready(function(){
-
-
+    
     $("#submitButt").click(function(){
 	//ASSERT: the sign up button has been clicked
 
-	console.log("Clicked!");
+	var checkedBox = document.getElementById("theCheckbox").checked;
 	
-	var name = document.getElementById("name").value;
-	var email = document.getElementById("email").value;
-	var password = document.getElementById("password").value;
-	//ASSERT: the input data taken from the name, email, password
-	
-	var userObject =
-	    {
-		"name": name,
-		"email": email,
-		"password": password
+	if (true) { 
+	    var name = document.getElementById("name").value;
+	    var email = document.getElementById("email").value;
+	    var password = document.getElementById("password").value;
+	    //ASSERT: the input data taken from the name, email, password
+	    
+	    var userObject =
+		{
+		    "name": name,
+		    "email": email,
+		    "password": password
+		}
+	    //ASSERT: information placed into a JSON object
+	    
+	    var userString = JSON.stringify(userObject);
+	    //ASSERT: the object is converted to a string
+	    
+	    $.ajax({
+		url: 'signupValid.php',
+		data: {'aUser': userString},
+		type: 'POST',
+		success: function(response){
+		    //alert(response);
+		    namespace.response = response;
+		}
+		//ASSERT: http request is made sending over
+		//        the user information
+	    });
+
+	    if (namespace.response == namespace.success) {
+		window.location.href = 'loggedIn.html';
 	    }
-	//ASSERT: information placed into a JSON object
-	
-	var userString = JSON.stringify(userObject);
-	//ASSERT: the object is converted to a string
-	
-	$.ajax({
-	    url: 'signupValid.php',
-	    data: {'aUser': userString},
-	    type: 'POST',
-	    success: function(response){
-		alert(response);
-	    }
-	    //ASSERT: http request is made sending over
-	    //        the user information
-	});
-	
+	}
+	else {
+	    alert("Please check disclaimer.");
+	}
+
     });
+			 
+    
 });
